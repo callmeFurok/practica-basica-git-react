9a0dbe1b9d523da205d56bf74e850061eb2070ef
Alfredo Catucuago <thefurok@gmail.com>
2022-01-11 10:58:44 -0500

    agregados los estilos usando BEM

---
+++ src/containers/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../assets/styles/Home.css";
+
+const Home = () => {
+    return (
+        <section className="home">
+            <h1 className="home__title"> Hola Mundo</h1>
+        </section>
+    );
+};
+
+export default Home;
