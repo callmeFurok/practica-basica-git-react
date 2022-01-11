2850b390018ee968b4dcd22f16918b6ba18d7a60
Alfredo Catucuago <thefurok@gmail.com>
2022-01-11 11:13:27 -0500

    creado modal para saludar

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import "../assets/styles/Home.css";
+import developer from "./../assets/img/developer.svg";
 
 const Home = () => {
     return (
         <section className="home">
-            <h1 className="home__title"> Hola Mundo</h1>
+            <a href="#target" className="cta"> Presiona para saludar</a>
+            <section className="modal" id="target">
+                <div className="modal__container">
+                    <a href="#" className="modal__close">X</a>
+                    <img src={developer} alt="Desarollador" className="modal__img"/>
+                    <div className="modal__texts">
+                        <h2 className="modal__title">Hola Mundo!! 😎</h2>
+                        <p className="modal__paragraph"> Alfredo - Pasante TI en TCS</p>
+                    </div>
+                </div>
+            </section>
         </section>
     );
 };
